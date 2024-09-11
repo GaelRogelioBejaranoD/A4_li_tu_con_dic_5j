0608cef8058c5053b44ccc331be499bf8642eb4e
Gael Rogelio Bejarano Dominguez <a.22308051281154@cbtis128.edu.mx>
2024-09-11 07:38:41 -0600

    Add files via upload

---
+++ mi_conjunto.py
@@ -0,0 +1,9 @@
+print("Ejemplo de conjuntos set")
+tipodeportes={"Fubol","Tennis","Baske"}
+print(tipodeportes)
+
+print("Lista de elementos del conjunto tipodeportes loop for")
+print(tipodeportes)
+for Bejarano in tipodeportes:
+    print(Bejarano)
+
